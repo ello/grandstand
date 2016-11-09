7ac683e0c20b3f08235720f319527e4c793b7f90
Jay Zeschin <jay.zeschin@modeset.com>
2016-11-08 17:02:20 -0700

    Correct migration semantics

--- db/migrate/20161101113557_add_brin_index_on_impressions_created_at.rb
+++ db/migrate/20161101113557_add_brin_index_on_impressions_created_at.rb
@@ -1,8 +1,11 @@
 class AddBrinIndexOnImpressionsCreatedAt < ActiveRecord::Migration[5.0]
   disable_ddl_transaction!
-  def change
+  def up
     unless index_exists?(:impressions, name: :index_impressions_brin_on_created_at)
       add_index :impressions, :created_at, name: :index_impressions_brin_on_created_at, using: :brin, algorithm: :concurrently
     end
+  end
+  def down
+    remove_index :impressions, :created_at, name: :index_impressions_brin_on_created_at
   end
 end
