8378d628d8854e3a51c742b138aa1059d0ee7d43
Justin Holmes <justin.ryan.holmes@icloud.com>
2017-11-02 17:00:13 -0600

    Handle responses properly if there are no post impressions
    - Very rare that this will ever be the case, but as a safe guard in case
    brands check out their dashboard in it’s zero state form

--- spec/requests/api/v1/artist_invites/total_spec.rb
+++ spec/requests/api/v1/artist_invites/total_spec.rb
@@ -18,16 +18,28 @@ RSpec.describe 'GET /api/v1/artist_invites/:id/total', type: :request do
   context 'Authenticated Request' do
     before do
       reset_aggregation_db
-      create_hourly_impressions
     end
 
     it 'should return total post impression count' do
+      create_hourly_impressions
+
       get "/api/v1/artist_invites/#{artist_invite_id}/total", headers: basic_auth_headers('user', 'password')
 
       expect(response.status).to eq(200)
       expect(response_json['data']).to eq({
         "artist_invite_id" => "1",
         "impressions" => 100,
+        "stream_kind" => nil,
+      })
+    end
+
+    it 'should return proper response with 0 impressions' do
+      get "/api/v1/artist_invites/#{artist_invite_id}/total", headers: basic_auth_headers('user', 'password')
+
+      expect(response.status).to eq(200)
+      expect(response_json['data']).to eq({
+        "artist_invite_id" => "1",
+        "impressions" => 0,
         "stream_kind" => nil,
       })
     end
